dfa6dd54f2d26a02f5ffd2846b87576f06d304bd
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-04-14 18:15:26 -0400

    react bootstrap for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,34 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to='/'>AMAZONX</Link>
+          <Navbar bg='dark' varient='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>AMAZONX</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path='/' element={<HomeScreen />} />
-            <Route path='/product/:slug' element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} />
+              <Route path='/product/:slug' element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>All Rights Reserved</div>
+        </footer>
       </div>
     </Router>
   );
