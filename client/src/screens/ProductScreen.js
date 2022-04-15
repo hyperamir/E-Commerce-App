d0b7f556e5850cc4c2b642a7906bb35c17d6005b
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-04-15 12:10:24 -0400

    add functionality to add to cart

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useReducer } from 'react';
+import { useContext, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -10,7 +10,8 @@ import Button from 'react-bootstrap/Button';
 import Rating from '../components/Rating';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
-import {getError} from '../utils'
+import { getError } from '../utils'
+import { Store } from '../Store';
 
 
 const reducer = (state, action) => {
@@ -47,6 +48,15 @@ function ProductScreen() {
     }
     fetchData();
   }, [slug]);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...product, quantity: 1 },
+    });
+  };
 
   return loading ? (
     <LoadingBox />
@@ -108,7 +118,7 @@ function ProductScreen() {
                   {product.countInStock > 0 && (
                     <ListGroup.Item>
                       <div className="d-grid">
-                        <Button variant="primary">Add to Cart</Button>
+                        <Button onClick={addToCartHandler} variant="primary">Add to Cart</Button>
                       </div>
                     </ListGroup.Item>
                   )}
