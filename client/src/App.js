be9941a6c81d6a6a169b3cb6ec2e565356ede115
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-04-16 10:03:55 -0400

    create signin component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
 import Badge from 'react-bootstrap/esm/Badge';
 import Nav from 'react-bootstrap/esm/Nav';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -41,6 +41,7 @@ function App() {
             <Routes>
               <Route path='/' element={<HomeScreen />} />
               <Route path='/cart' element={<CartScreen />} />
+              <Route path='/signin' element={<SigninScreen />} />
               <Route path='/product/:slug' element={<ProductScreen />} />
             </Routes>
           </Container>
